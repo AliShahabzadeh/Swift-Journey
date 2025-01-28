f326a60f2061eddf0b98b9247e32bf862c5fc93a
Ali Shahabzadeh <129598985+AliShahabzadeh@users.noreply.github.com>
2025-01-28 14:35:40 +0100

    Update PreferredLocalPrint.swift
    fix: SAMPLE OUTPUT Result

--- src/PreferredLocalPrint.swift
+++ src/PreferredLocalPrint.swift
@@ -20,9 +20,9 @@ for i in preferredLanguages{
 /*
 SAMPLE OUTPUT:
 
-Your preferred language no. 0 is: en-US
-Your preferred language no. 1 is: it-US
-Your preferred language no. 2 is: fa-US
-Your preferred language no. 3 is: ru-US
-Your preferred language no. 4 is: de-US
+Your preferred language no. 1 is: en-US
+Your preferred language no. 2 is: it-US
+Your preferred language no. 3 is: fa-US
+Your preferred language no. 4 is: ru-US
+Your preferred language no. 5 is: de-US
 */
