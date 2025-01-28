255eecb3f18483afb437822259fd8788aae47782
Ali Shahabzadeh <129598985+AliShahabzadeh@users.noreply.github.com>
2025-01-28 13:20:59 +0100

    Add files via upload
    Simple Swift Function

---
+++ src/FirstFunction.swift
@@ -0,0 +1,38 @@
+func incrementByUnit(_ number: Int, _ unit: Int) -> Int {
+
+  return number + unit
+
+}
+
+print("Enter a number: ")
+
+if let input = readLine(), let number = Int(input){
+
+  print("Enter a unit: ")
+
+  if let input = readLine(), let unit = Int(input){
+
+    let finalNumber = incrementByUnit(number, unit)
+
+    print("Your final Number is \(finalNumber)")
+
+  } else {
+
+    print("Unit Format Error!")
+
+  }
+
+} else {
+
+  print("Number Format Error!")
+
+}
+
+/*
+SAMPLE OUTPUT:
+
+> 5
+> 2
+
+Your final Number is 7
+*/
