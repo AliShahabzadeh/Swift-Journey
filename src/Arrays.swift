fb1b1a52fb8647e3fed5ae39f3ab03824b991eaf
Ali Shahabzadeh <129598985+AliShahabzadeh@users.noreply.github.com>
2025-01-30 17:02:01 +0100

    Add files via upload

---
+++ src/Arrays.swift
@@ -0,0 +1,22 @@
+//
+//  PrintOdd_While.swift
+//  Swift-Journey
+//
+//  Created by Ali Shahabzadeh on 30/01/2025.
+//
+
+var names = ["Ali", "Andrea", "Benjamin"]
+
+for i in names{
+
+  print(i)
+
+}
+
+/*
+OUTPUT:
+
+Ali
+Andrea
+Benjamin
+*/
